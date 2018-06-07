971c5065785aadeb195c79039bf74472e728a532
amanda johnson <manda.panda91@yahoo.com>
2018-06-07 23:41:46 +0000

    added signup routes and view for users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,18 @@
 class UserController < ApplicationController
  get '/signup' do
-   
+   if !logged_in
+     erb :'/users/signup'
+   else
+     redirect '/workouts'
+   end
+ end
+
+ post '/signup' do
+   if @user = User.create(params)
+     session[:user_id] = @user.id
+     redirect '/workouts'
+   else
+     redirect '/signup'
+   end
  end
 end
