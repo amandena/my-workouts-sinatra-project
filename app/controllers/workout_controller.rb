df6473cf9fc9cebfb2a9b202ac76967d0247f8dc
amanda johnson <manda.panda91@yahoo.com>
2018-06-12 02:04:54 +0000

    added routes to create new workout and create_workout view

--- app/controllers/workout_controller.rb
+++ app/controllers/workout_controller.rb
@@ -7,4 +7,24 @@ class WorkoutController < ApplicationController
       redirect '/login'
     end
   end
+
+  get '/workouts/new' do
+    if logged_in
+      erb :'/workouts/create_workout'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/workouts' do
+    if logged_in
+      if @workout = current_user.workouts.create(params)
+        redirect "/workouts/#{@workout.id}"
+      else
+        redirect '/workouts/new'
+      end
+    else
+      redirect '/login'
+    end
+  end
 end
