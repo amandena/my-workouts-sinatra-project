f013ce76f4996f003b49029abcbbf195520a058c
amanda johnson <manda.panda91@yahoo.com>
2018-06-12 01:46:00 +0000

    added validators to workout model

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,7 @@
 class Workout < ActiveRecord::Base
-  belongs_to :user 
+  belongs_to :user
+  validates :exercise_name, presence: true
+  validates :minutes, presence: true
+  validates :water_intake, presence: true
+  validates :date, presence: true
 end
