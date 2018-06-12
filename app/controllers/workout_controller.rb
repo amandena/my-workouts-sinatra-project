11bbfb0029cb996c9df0578695b3e4f7c119e450
amanda johnson <manda.panda91@yahoo.com>
2018-06-12 01:31:55 +0000

    added workout route and view

--- app/controllers/workout_controller.rb
+++ app/controllers/workout_controller.rb
@@ -1,5 +1,10 @@
 class WorkoutController < ApplicationController
   get '/workouts' do
-
+    if logged_in
+      @workouts = Workout.all
+      erb :'/workouts/workouts'
+    else
+      redirect '/login'
+    end
   end
 end
