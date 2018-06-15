0d9c477f2cb321a0edfac1823837f51361b18e74
amanda johnson <manda.panda91@yahoo.com>
2018-06-15 22:36:41 +0000

    added edit routes and edit_workout view

--- app/controllers/workout_controller.rb
+++ app/controllers/workout_controller.rb
@@ -36,4 +36,30 @@ class WorkoutController < ApplicationController
       redirect '/login'
     end
   end
+
+  get '/workouts/:id/edit' do
+    if logged_in
+      @workout = Workout.find_by_id(params[:id])
+      erb :'/workouts/edit_workout'
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/workouts/:id' do
+    if logged_in
+      @workout = Workout.find_by_id(params[:id])
+      if @workout && @workout.user == current_user
+        if @workout.update(exercise_name: params[:exercise_name], minutes: params[:minutes], water_intake: params[:water_intake], date: params[:date])
+          redirect "/workouts/#{@workout.id}"
+        else
+          redirect "/workouts/#{@workout.id}/edit"
+        end
+      else
+        redirect '/workouts'
+      end
+    else
+      redirect '/login'
+    end
+  end
 end
