926fb119ccf918ff11541eefacb18fa60847fe60
amanda johnson <manda.panda91@yahoo.com>
2018-06-19 23:51:10 +0000

    refactored workout model

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,7 +1,3 @@
 class Workout < ActiveRecord::Base
   belongs_to :user
-  validates :exercise_name, presence: true
-  validates :minutes, presence: true
-  validates :water_intake, presence: true
-  validates :date, presence: true
 end
